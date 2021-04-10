5c5405bbc0039438a677796f0b9cb71e2d5ba445
ryabovArt <79091451+ryabovArt@users.noreply.github.com>
2021-04-10 20:55:28 +0300

    Menu

---
+++ Assets/Puzzle/MoveRectangle.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveRectangle : MonoBehaviour
+{
+    public GameObject left;
+    public GameObject right;
+    public GameObject plane;
+    Vector2 planeSize;
+
+    private float startPosition;
+    private bool mouseDown = false;
+    private Vector2 screenBorder1, screenBorder2;
+    private Vector2 spriteSize;
+
+    private void Start()
+    {
+        planeSize = plane.GetComponent<RectTransform>().sizeDelta;
+        if (gameObject.CompareTag("Horizontal"))
+            startPosition = GetComponent<Transform>().position.y;
+        if (gameObject.CompareTag("Vertical"))
+            startPosition = GetComponent<Transform>().position.x;
+        screenBorder1 = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height));
+        screenBorder2 = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        spriteSize = GetComponent<SpriteRenderer>().bounds.size;
+        Debug.Log(planeSize);
+    }
+
+    private void OnMouseDown()
+    {
+        mouseDown = true;
+    }
+
+    private void OnMouseUp()
+    {
+        mouseDown = !mouseDown;
+    }
+
+    private void Update()
+    {
+        Vector3 cursor = Input.mousePosition;
+        cursor = Camera.main.ScreenToWorldPoint(cursor);
+        //Debug.Log(cursor);
+        cursor.z = 0;
+        if (gameObject.CompareTag("Horizontal"))
+        {
+            cursor.y = startPosition;
+            if (transform.position.x - spriteSize.x * 2 < screenBorder1.x || transform.position.x + spriteSize.x * 2 > screenBorder2.x)
+            {
+                //cursor.x = Mathf.Clamp(cursor.x, screenBorder1.x, screenBorder2.x - (spriteSize.x + spriteSize.x / 2));
+                cursor.x = Mathf.Clamp(cursor.x, screenBorder1.x + spriteSize.x / 3, screenBorder2.x - spriteSize.x / 2);
+                //Debug.Log("!!!");
+            }
+        }
+        if (gameObject.CompareTag("Vertical"))
+            cursor.x = startPosition;
+
+        if (mouseDown)
+        {
+            this.transform.position = cursor;
+        }
+
+        
+        //if (transform.position.y > border2.y || transform.position.x < border1.y)
+        //{
+        //    Debug.Log("!!!");
+        //}
+    }
+
+}
---
+++ Assets/Puzzle/Test.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector2 planeSize;
+    private RectTransform rectTransform;
+    public Sprite sprite;
+    public int[,] grid;
+    int vertical, horizontal, columns, rows;
+    int xPos;
+    int yPos;
+
+    void Start()
+    {
+        xPos = Mathf.RoundToInt(transform.position.x);
+        yPos = Mathf.RoundToInt(transform.position.y);
+
+        //vertical = (int)Camera.main.orthographicSize;
+        planeSize = plane.GetComponent<RectTransform>().sizeDelta;
+        //Debug.Log(planeSize);
+        vertical = Mathf.RoundToInt(planeSize.y);
+        Debug.Log(vertical);
+        horizontal = (vertical * ((int)planeSize.x / (int)planeSize.y));
+        
+        //horizontal = vertical * (Screen.width / Screen.height);
+
+        columns = horizontal * 2;
+        rows = vertical * 2;
+        grid = new int[columns, rows];
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                grid[i, j] = Random.Range(0, 10);
+                SpawnTile(i, j, grid[i, j]);
+            }
+        }
+    }
+
+    private void SpawnTile(int x, int y, int value)
+    {
+        GameObject go = new GameObject();
+        go.transform.position = new Vector2 (x - (horizontal - 0.5f), y - (vertical - 0.5f));
+        go.AddComponent<SpriteRenderer>();
+        var s = go.GetComponent<SpriteRenderer>();
+        s.sprite = sprite;
+        s.color = new Color(value, value, value);
+    }
+}
