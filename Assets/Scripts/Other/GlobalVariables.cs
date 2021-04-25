179da494e1e926007f693330b7d04e96e534bbe1
Demorok <grabmon007@yandex.ru>
2021-04-25 16:15:21 +0300

    pause button & panel

---
+++ Assets/Scripts/Other/GlobalVariables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalVariables
+{
+    //scene indexes
+    public const int MAINMENUSCENEINDEX = 0;
+    public const int LEVEL1SCENEINDEX = 1;
+    public const int ROOMSCENEINDEX = 2;
+}
---
+++ Assets/Scripts/UI/PausePanel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausePanel : MonoBehaviour
+{
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Back_to_Main_Menu()
+    {
+        SceneManager.LoadScene(GlobalVariables.MAINMENUSCENEINDEX);
+    }
+
+}
