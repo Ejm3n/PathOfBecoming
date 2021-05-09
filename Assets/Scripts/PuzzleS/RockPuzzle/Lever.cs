971e13b74b2a757e7227a92f89762d90ca1fb20c
Ejm3n <yashkaydakov@yandex.ru>
2021-05-09 13:00:08 +0300

    добавил рок пазл

---
+++ Assets/Scripts/PuzzleS/RockPuzzle/Lever.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+   // [SerializeField] int leverNum;
+    [SerializeField] int rockDown;
+    [SerializeField] int rockUp;
+    [SerializeField] RockController RC;
+    private void OnMouseUp()
+    {
+        RC.ChangeRockPositions(rockUp, rockDown);
+    }
+    
+}
