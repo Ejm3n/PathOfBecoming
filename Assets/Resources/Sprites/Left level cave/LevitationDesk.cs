d69a0470fa0156596eb948205128af3271ae1df4
Ejm3n <yashkaydakov@yandex.ru>
2021-07-05 20:39:16 +0300

    инвентарь и книга

---
+++ Assets/Resources/Sprites/Left level cave/LevitationDesk.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevitationDesk : MonoBehaviour
+{
+    bool moveto2pos;
+    Vector3 pos1 = new Vector3(3.48f, -2.13f);
+    Vector3 pos2 = new Vector3(-0.3f,-2.78f);
+    float speed = 0.01f;
+    void Update()
+    {
+        //if(moveto2pos && transform.position != pos2)
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position,pos2,speed);
+        //}
+        //else if(transform.position == pos2)
+        //{
+        //    moveto2pos = false;
+        //}
+        //if(!moveto2pos && transform.position != pos1)
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position, pos1, speed);
+        //}
+        //else if(transform.position == pos1)
+        //{
+        //    moveto2pos = true;
+        //}
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            collision.transform.parent = transform;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.parent = null;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -26,7 +26,7 @@ public class PlayerAnimator : MonoBehaviour
         else
         {
             anim.SetBool("walk", false);
-            SoundManager.instance.Steps();
+           // SoundManager.instance.Steps();
         }
 
         if (!PC.isGround)
