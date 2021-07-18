36a67eebb9cc97972a5559d350a4d77061376e6f
Demorok <grabmon007@yandex.ru>
2021-07-18 15:56:06 +0300

    add brick puzzle on door

---
+++ Assets/Scripts/Level obj/SliderPuzzleDoor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using AnimationUtils.TransformUtils;
+
+public class SliderPuzzleDoor : MonoBehaviour
+{
+    public void Open_Door()
+    {
+        Vector3 offset = transform.position + Vector3.right * 3;
+        transform.Move_To(offset, 0.3f);
+    }
+}
