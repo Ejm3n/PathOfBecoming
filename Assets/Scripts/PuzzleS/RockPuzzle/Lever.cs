2db498dcced52234197e87512756c53bc81c6087
Ejm3n <yashkaydakov@yandex.ru>
2021-07-22 22:30:03 +0300

    leversFixed

--- Assets/Scripts/PuzzleS/RockPuzzle/Lever.cs
+++ Assets/Scripts/PuzzleS/RockPuzzle/Lever.cs
@@ -10,6 +10,7 @@ public class Lever : InteractEvent
     [SerializeField] RockController RC;
     public override void Start_Event()
     {
+        Debug.Log("PRESSED ON BRANCH & down - " + rockDown + " up - " + rockUp);
         if(!RC.ended)
             RC.ChangeRockPositions(rockUp, rockDown);
     }
