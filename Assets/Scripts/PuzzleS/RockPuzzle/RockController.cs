4f4ccee86eb420619bfbb89ae78d713b5cd660c2
Demorok <grabmon007@yandex.ru>
2021-08-05 20:44:35 +0300

    rock puzzle fix

--- Assets/Scripts/PuzzleS/RockPuzzle/RockController.cs
+++ Assets/Scripts/PuzzleS/RockPuzzle/RockController.cs
@@ -29,12 +29,12 @@ public class RockController : MonoBehaviour
     {
         if(playerNear)
         {
-            if (rocks[whatRockUp].position.y < -0.8f)
+            if (rocks[whatRockUp].localPosition.y < 1.85f)
             {
                 rockLevelChecker[whatRockUp]++;
                 rocks[whatRockUp].position = new Vector3(rocks[whatRockUp].position.x, rocks[whatRockUp].position.y + 1.2f, rocks[whatRockUp].position.z);
             }
-            if (rocks[whatRockDown].position.y > -4.4f)
+            if (rocks[whatRockDown].localPosition.y > -1.75f)
             {
                 rockLevelChecker[whatRockDown]--;
                 rocks[whatRockDown].position = new Vector3(rocks[whatRockDown].position.x, rocks[whatRockDown].position.y - 1.2f, rocks[whatRockDown].position.z);
