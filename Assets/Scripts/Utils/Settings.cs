6a7936b00908bd4d79e8b7a938236f49911de320
Demorok <grabmon007@yandex.ru>
2021-10-15 23:43:35 +0300

    added control buttons

---
+++ Assets/Scripts/Utils/Settings.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Settings
+{
+    public static class PCControlButtons
+    {
+        public static KeyCode LEFT = KeyCode.A;
+        public static KeyCode RIGHT = KeyCode.D;
+        public static KeyCode RUN = KeyCode.LeftShift;
+
+        public static KeyCode JUMPandUP = KeyCode.W;
+
+        public static KeyCode INVENTORYandDOWN = KeyCode.S;
+
+        public static KeyCode SWITCHSPELL = KeyCode.Q;
+
+        public static KeyCode USESPELL = KeyCode.Space;
+
+        public static KeyCode INTERACT = KeyCode.E;
+    }
+}
