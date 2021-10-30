43519b7c0c8ca7df3c7f3d019f8c72b6c364dda5
Ejm3n <yashaydakov@mail.ru>
2021-10-30 18:43:44 +0300

    AddedAltarOFFlame

---
+++ Assets/Scripts/PuzzleS/AltarOfFlame/BlackBlock.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBlock : Block
+{
+    [SerializeField] Block[] cancellingBlocks;
+    BlockPuzzle bp;
+    bool isStealingMana = false;
+    private void Awake()
+    {
+        bp = FindObjectOfType<BlockPuzzle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isFilled && !isStealingMana)
+        {
+            foreach (Block block in cancellingBlocks)
+            {
+                if(block.isFilled)
+                {
+                    isStealingMana = true;
+                    StartCoroutine( EatMana(block));
+                    break;
+                }
+            }
+        }
+    }
+    IEnumerator EatMana(Block block)
+    {
+        bp.AllowOrNoClicks(false);
+        //block.PaintBlock(Color.yellow);
+        Debug.Log("tut3");
+        yield return new WaitForSeconds(.5f);
+        block.PaintBlock(Color.red);
+        yield return new WaitForSeconds(.5f);
+
+        block.EmptyBlock();
+        bp.AllowOrNoClicks(true);
+        isStealingMana = false;
+    }
+}
---
+++ Assets/Scripts/PuzzleS/AltarOfFlame/Block.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] Color defaultColor = Color.white;
+    SpriteRenderer sp;
+    public bool isFilled = false;
+    public int manacost;
+    private int defaultMana;
+    public bool HaveRazlom;
+    public int StolbToFill;
+    private Stolb stolb;
+    void Start()
+    {
+        sp = gameObject.GetComponent<SpriteRenderer>();
+        stolb = GetComponentInParent<Stolb>();
+        defaultMana = manacost;
+    }
+
+    public int FillBlock(int currentMana)
+    {
+        currentMana -= manacost;
+        if (currentMana < 0)
+            currentMana = 0;
+        sp.color = Color.yellow;
+        isFilled = true;
+        if(HaveRazlom)
+        {
+            stolb.StartRazlom(this);
+        }
+        return currentMana;
+    }
+    public void EmptyBlock()
+    {
+        manacost = defaultMana;
+        sp.color = defaultColor;
+        isFilled = false ;
+    }
+    public void PartFillBlock(int strength)
+    {
+
+        sp.color = Color.green;
+        manacost -= strength;
+        if(manacost<=0)
+        {
+            int i = 0;
+            FillBlock(i);
+        }
+    }
+    public void PaintBlock(Color color)
+    {
+        sp.color = color;
+    }
+}
