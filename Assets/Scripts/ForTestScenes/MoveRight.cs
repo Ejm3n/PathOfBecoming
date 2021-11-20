451f173b7b2f555a17ea20108559a76335733b2d
Ejm3n <yashaydakov@mail.ru>
2021-11-21 00:03:45 +0200

    river scene add

---
+++ Assets/Scripts/ForTestScenes/MoveRight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRight : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,new Vector2(transform.position.x+10f,transform.position.y),speed);
+    }
+}
