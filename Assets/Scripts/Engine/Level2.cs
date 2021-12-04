47713aac89fdea236d3f61731080550a04ff2f1d
Demorok <grabmon007@yandex.ru>
2021-12-04 19:40:06 +0300

    place for mushrooms

--- Assets/Scripts/Engine/Level2.cs
+++ Assets/Scripts/Engine/Level2.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 
 public class Level2 : Engine
 {
-    private int _mushroom_count = 0;
-    private const int need_mushrooms = 4;
-
     protected override void Awake()
     {
         mainTheme = Resources.Load<AudioClip>("Sounds/Music/Level2");
@@ -19,20 +16,12 @@ public class Level2 : Engine
         Spawn_Characters(player, fairy);
         Connect_Fairy_to_Player();
         userInterface.spellBook.Learn_Spell(Resources.Load<GameObject>("Prefabs/Magic/Spells/Whirlwind(Spell)"));
+        userInterface.spellBook.Learn_Spell(Resources.Load<GameObject>("Prefabs/Magic/Spells/Lighter(Spell)"));
         Show_Scene(() => {
             userInterface.inventory.Enable_Inventory();
             userInterface.spellBook.Enable_Spellbook();
             userInterface.Enable_Interface(true);
             startDialog.SetActive(true);
         });
-    }
-
-    public void Add_Mushroom()
-    {
-        ++_mushroom_count;
-        if (_mushroom_count >= need_mushrooms)
-        {
-
-        }
     }
 }
