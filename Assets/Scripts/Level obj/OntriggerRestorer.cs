201d7c6b8e1b46d71a1acc30849639ababc6bac9
Ejm3n <yashaydakov@mail.ru>
2022-02-14 15:29:54 +0200

    fixed peschernik

---
+++ Assets/Scripts/Level obj/OntriggerRestorer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OntriggerRestorer : MonoBehaviour
+{
+    [SerializeField] private OnTriggerActivation trigger;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        trigger.RestoreTrigger();
+    }
+}
