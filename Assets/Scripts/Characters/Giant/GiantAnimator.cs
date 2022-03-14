2d7f48f33182500129826183de59beaa77e2f859
Ejm3n <yashaydakov@mail.ru>
2022-03-14 23:44:11 +0200

    giant sounds

--- Assets/Scripts/Characters/Giant/GiantAnimator.cs
+++ Assets/Scripts/Characters/Giant/GiantAnimator.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class GiantAnimator : MonoBehaviour
 {
     [SerializeField] private GameObject platformForMushroom;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip sneezeSound;
+    [SerializeField] private AudioClip rumblingSound;
+    [SerializeField] private AudioClip shape;
+    [SerializeField] private AudioClip shapeOff;
     private Animator animator;
 
     private void Awake()
@@ -21,18 +26,22 @@ public class GiantAnimator : MonoBehaviour
     }
     public void ChangeToIdle()
     {
+        audioSource.PlayOneShot(shape);
         animator.SetTrigger("Idle");
     }
     public void ChangeToShutting()
     {
+        audioSource.PlayOneShot(shapeOff);
         animator.SetTrigger("Shutting");
     }
     public void ChangeToShiver()
     {
+        audioSource.PlayOneShot(rumblingSound);
         animator.SetTrigger("Shiver");
     }
     public void ChangeToSneeze()
     {
+        audioSource.PlayOneShot(sneezeSound);
         animator.SetTrigger("Sneeze");
     }
     public void MushroomFalling()
