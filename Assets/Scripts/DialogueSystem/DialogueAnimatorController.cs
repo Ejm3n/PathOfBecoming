4f4a5c82c903470de4018ca9f67104c1476f94d9
Ejm3n <yashaydakov@mail.ru>
2022-03-18 21:36:14 +0200

    openingDialogueSound

---
+++ Assets/Scripts/DialogueSystem/DialogueAnimatorController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueAnimatorController : MonoBehaviour
+{
+    [SerializeField] private AudioClip openingPanel;
+    public void PlayOpeningSound()
+    {
+        SoundRecorder.Play_Effect(openingPanel);
+    }
+}
