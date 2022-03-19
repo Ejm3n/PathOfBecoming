9106b54062742c309dfe1b65250cdb5a9bfa4183
B0rrow <maxutka94@gmail.com>
2022-03-20 00:37:13 +0300

    zvooq and death

---
+++ Assets/Scripts/Level obj/DeathZone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Engine.current.playerController.Die();
+    }
+}
